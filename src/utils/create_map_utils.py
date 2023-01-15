194387ba729b8fe61761287ccc38ed743157a3fe
thomashsimm <thomas.simm@gmail.com>
2023-01-15 19:07:03 +0000

    More edits, not quite there need to do mains and other bits

--- src/utils/create_map_utils.py
+++ src/utils/create_map_utils.py
@@ -40,6 +40,18 @@ wales_region_dict={
      
     }
     
+def main():
+    """
+    Creates:
+        - A pandas dataframe with values for each postcode and an area-name to plot to
+        - Create a json file with area-name and associated polygons
+    These are then saved to files
+    """    
+    
+    
+    
+    
+    
 def load_kml(url_data):
     """
     loads a KML location file (from local file or url) 
@@ -273,77 +285,3 @@ def _postcodeDF_additionalProcessing(postcode_df):
     postcode_df = postcode_df[cols_to_use]
     
     return postcode_df
-
-
-
-def plot_map(df, 
-             what_to_plot='amount_pc',region_to_plot='Name',
-             json_path='./_data/region.json',
-             longitude=-3.1, latitude=54.1):
-    """
-    loads a KML location file (from local file or url) 
-    and then saves this as a json file locally
-    
-    Args: df (pandas dataframe) dataframe with location (region_to_plot) related to json file
-                in one column and data to plot (what_to_plot) in another
-          what_to_plot/region_to_plot (string) columns in df
-          json_path (file path) the path to the json file
-          longitude/latitude (float) for the centre of the map
-    Returns: 
-        The folium map
-    
-    """
-    # create a basic map
-    # This sets the basic look of the map
-    # things like the zoom, color scheme, where the map is centred etc
-
-    m = folium.Map(location=[latitude,longitude], 
-                   zoom_start=5,
-                   control_scale=True,
-                   tiles="Stamen Toner")
-    
-    folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
-    
-    """
-    Create the Choropleth part:
-
-    - This creates the Choropleth map (i.e. regions on a map of different color based on their value)
-    - json_path this is the path to a json file with the location data
-        - key_on= "feature.properties.Name" refers to the json file
-        - and needs to match region_to_plot in columns=[region_to_plot,what_to_plot]
-    - data the data normally a pandas dataframe
-        - the columns are region_to_plot and what_to_plot
-        - what_to_plot are the values to plot
-        - region_to_plot should match the json file
-    """
-
-    choropleth = folium.Choropleth(
-        geo_data=json_path,
-        name='choropleth',
-        legend_name= what_to_plot,
-        data= df,
-        columns=[region_to_plot,what_to_plot],
-        key_on= "feature.properties.Name",
-        fill_color='YlGn',
-    ).add_to(m)
-    
-    # adds ability to see names of regions on mouseove
-    choropleth.geojson.add_child(
-    folium.features.GeoJsonTooltip(['Name'],labels=False)
-    )
-    
-    return m
-
-def download_zip(url_data):
-    from io import BytesIO
-    from zipfile import ZipFile
-    from urllib.request import urlopen
-
-    resp = urlopen(url_data)
-    myzip = ZipFile(BytesIO(resp.read()))
-    filepath = [file for file in myzip.namelist() if file.split('.')[-1]=='kml'][0]
-    myzip.extract(filepath, path="./_data")
-    
-    path_data = os.path.join(".\\_data", filepath)
-    
-    return path_data
