654e7621dda5153f9c7a8b9b4de7e36dcdaebfc5
thomashsimm <thomas.simm@gmail.com>
2023-01-16 23:16:06 +0000

    create_postcode_lookup works now, and an updated postcode lookup file

--- src/utils/create_postcode_lookup.py
+++ src/utils/create_postcode_lookup.py
@@ -1,25 +1,98 @@
 import os
 import pandas as pd
 import copy
+import re
 
+import sys
+MAIN_PATH = os.getcwd()
+sys.path.append(MAIN_PATH)
 
+import src.data_cleaning.modify_dfs as md
 
-def main():
+import src.utils.map_utils as mpu
+import src.data_loading.loads_from_url as lfu
+from src.data_loading.loads_from_url import get_poscode_df
+
+def main(data_file = './data/combined_df.csv'):
+    """
+    main function creates a new postcode-area mapper file 
+    by searching for postcodes with less characters
     
-    # load data-file
-    
+    """  
+    # load data
+    df = pd.read_csv(data_file,index_col=0)
+
     # load postcode mapper
+    postcode_df = get_poscode_df(path_to_csv= ".//src//utils//map_data//postcode_to_region.csv",
+                    usecols=['Postcode','mapArea'])
+    
+    # get a list of postcode data with characters removed
+    # i.e. shorter_pc_list[0] = get_poscode_df()
+    #      shorter_pc_list[0] = get_poscode_df( for postcode reduced by one char)
+    shorter_pc_list = get_postcode_list(postcode_df,usecols=['Postcode','mapArea'],
+                    reduce_p_max=4)
+
+    # get the columns in the data containing postcode data
+    postcode_cols_in_df = [col for col in df.columns if re.search(r'[Pp]ostcode',col)]
+
+    for postcode_col in postcode_cols_in_df:
+        print(postcode_col)
+        postcode_df = addto_pcode_mapper(df, shorter_pc_list,
+                            postcode_column = postcode_col)
+        shorter_pc_list[0] = postcode_df
+
+    # save the new file
+    path_to_new_csv= ".//src//utils//map_data//postcode_to_region_new.csv"
+    postcode_df.to_csv(path_to_new_csv)
+
+def get_postcode_list(postcodes_df, usecols=['Postcode','mapArea'],
+                    reduce_p_max=4
+                    ):
+    """
+    loads poscode data and returns that as a list of dataframes
+    where postcode has has been reduced by 1:reduce_p_max-1 in length
+    
+    Args:   path_to_csv (path) csv file of postcode data
+            usecols (list[str]) what columns to load, also includes latitude,longitude
+            reduce_p_max (int) number of chars to take off end of potscode -1
+    Returns: 
+        list_postcodes_df (list[pd.DataFrame]) list of pandas dataframe of the postcode data
+    
+    """   
+    postcodes_df = postcodes_df[usecols]
+    postcodes_df_temp = postcodes_df.copy()
+    
+    list_postcodes_df = []
+    list_postcodes_df.append( postcodes_df_temp.copy() )
+    
+    for i in range(1,reduce_p_max):
+        colname = f"Postcode_m{i}"
+        postcodes_df_temp[colname] = postcodes_df_temp['Postcode'].str[:-i]    
+        
+        df = _getpc(postcodes_df_temp, colname)
+        df = df.rename(columns={colname:'Postcode'})
+        list_postcodes_df.append( df )
     
     
+    return list_postcodes_df
 
-
-def addto_pcode_mapper(df, MAIN_PATH, shorter_pc_list,postcode_column = 'address_postcode'):
-
-    # load postcode to area df
-    map_data_file = os.path.join(MAIN_PATH, "src", "utils", "map_data", "postcode_to_region.csv")
-    postcodes_to_area_df = mpu.get_poscode_df(path_to_csv = map_data_file)
-    postcodes_to_area_df['removeChar']=0
+def addto_pcode_mapper(df, shorter_pc_list,
+                    postcode_column = 'address_postcode'):
+    """
+    the main function creates a new postcode-area mapper from data inputted
+    using a basic algorithm of reducing the number of chars in a postcode
     
+    Args:   df (pd.DataFrame) the data with a postcode column
+            shorter_pc_list (list[pd.DataFrame]) list of dataframes to match postcodes to areas
+                each list reduces the characters in the postcode
+            postcode_column (str) name of column in df containg postcode
+    Returns: 
+            postcodes_to_area_df (pd.DataFrame) an updated postcode-area mapper
+    """  
+    
+    # First run through for the full-postcode    
+    postcodes_to_area_df = shorter_pc_list[0]
+    postcodes_to_area_df['removeChar'] = 0
     
     df_pc= copy.deepcopy(df)
 
@@ -31,7 +104,7 @@ def addto_pcode_mapper(df, MAIN_PATH, shorter_pc_list,postcode_column = 'address
 
     # check if already in the postcode to area df
     # just use the ones that are not
-    df_pc['mapArea'] = _create_area_col(df_pc, postcodes_to_area_df, 'Postcode')
+    df_pc['mapArea'] = _create_area_col(df_pc, postcodes_to_area_df[['Postcode','mapArea']], 'Postcode')
     df_pc = df_pc.loc[df_pc['mapArea'].isna()]
 
     print(f"After initial mapping\n \
@@ -62,32 +135,8 @@ and {len(df_pc)} still unknonwn")
         
         print()
         
-    return postcodes_to_area_df, df_add, df_pc
+    return postcodes_to_area_df
         
-newPC, df_add, df_pc = dothis(df, MAIN_PATH, shorter_pc_list)
-
-def _postcode_to_area(df_,
-                    postcodes_df, 
-                    postcode_column='Postcode'):
-    """
-    loads poscode data and returns an array of the area the postcode corresponds to
-         by mapping to postcodes_df
-    
-    Args:   path_to_csv (path) csv file of postcode data
-            usecols (list[str]) what columns to load, also includes latitude,longitude
-            reduce_p_max (int) number of chars to take off end of potscode -1
-    Returns: 
-        df_['mapArea'] (column of pd.DataFrame) 
-    
-    """  
-    
-    df_ = copy.deepcopy(df)
-    postcode_dict = dict(postcodes_df[['Postcode','mapArea']].values)
-    
-    df_['mapArea'] = df_.loc[:,postcode_column].map(postcode_dict)
-
-    return df_['mapArea']
-
 
 
 def _getpc(postcodes_df_temp, colname):
@@ -109,36 +158,13 @@ def _getpc(postcodes_df_temp, colname):
     postcode_short = postcode_short.loc[:,[colname,'mapArea']].reset_index(drop=True)
     return postcode_short
 
-def get_postcode_list(path_to_csv= "..//src//utils//map_data//postcode_to_region.csv",
-                    usecols=['Postcode','mapArea'],
-                    reduce_p_max=4
-                    ):
-    """
-    loads poscode data and returns that as a list of dataframes
-    where postcode has has been reduced by 1:reduce_p_max-1 in length
-    
-    Args:   path_to_csv (path) csv file of postcode data
-            usecols (list[str]) what columns to load, also includes latitude,longitude
-            reduce_p_max (int) number of chars to take off end of potscode -1
-    Returns: 
-        list_postcodes_df (list[pd.DataFrame]) list of pandas dataframe of the postcode data
-    
-    """   
 
-    postcodes_df = get_poscode_df(path_to_csv, usecols)
+def _create_area_col(df, postcodes_df, postcode_column):
+    df_ =copy.deepcopy(df)
+    postcode_dict = dict(postcodes_df[['Postcode','mapArea']].values)
+    
+    df_['mapArea'] = df_.loc[:,postcode_column].map(postcode_dict)
+    return df_['mapArea']
 
-    postcodes_df_temp = postcodes_df.copy()
-    
-    list_postcodes_df = []
-    list_postcodes_df.append( postcodes_df_temp.copy() )
-    
-    for i in range(1,reduce_p_max):
-        colname = f"Postcode_m{i}"
-        postcodes_df_temp[colname] = postcodes_df_temp['Postcode'].str[:-i]    
-        
-        df = _getpc(postcodes_df_temp, colname)
-        df = df.rename(columns={colname:'Postcode'})
-        list_postcodes_df.append( df )
-    
-    
-    return list_postcodes_df
+if __name__ == '__main__':
+    main() 
